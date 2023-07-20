8b11d9053b40b0f608f240ce7d7dfd3a5f3d81ad
rana <rana.ahammed.012@gmail.com>
2023-07-20 20:08:39 +0600

    third

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDatabase = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
         console.log('Database connected successfully');
     } catch (error) {
         console.log('Error while connecting with the database');
