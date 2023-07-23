c504c3eeb0800303ff99e9483312872dacf10f96
rana <rana.ahammed.012@gmail.com>
2023-07-23 12:49:10 +0600

    .

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -108,9 +108,10 @@ export const login = async (req, res) => {
         const jwtToken = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
         const options = {
             expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-            httpOnly: true,
             sameSite: 'none',
+            domain: '.vercel.app',
             secure: true
+            // path: '/'
         };
 
         res.status(200)
